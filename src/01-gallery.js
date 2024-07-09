d038154baea80f9f14216d71fb27fa7cd900793a
KarolinaSkawinska <karolinaa1607@gmail.com>
2024-07-09 09:29:46 +0200

    Create 01-gallery.js

---
+++ src/01-gallery.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles.css" />
+
+    <title>Gallery</title>
+
+  </head>
+  <body>
+    <ul class="gallery">
+<li class="gallery-item">
+	<a class="gallery-link" href="large-image.jpg">
+		<img 
+			class="gallery-image" 
+			src="small-image.jpg" 
+			alt="Image description" 
+			/>
+	</a>
+</li>
+</ul>
+  </body>
+</html>
